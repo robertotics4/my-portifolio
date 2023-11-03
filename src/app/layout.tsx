287d4b6a7ba7fc241af8f287aff9fa40ed4a8a01
robertotics4 <robertooliveiragp@gmail.com>
2023-11-03 00:59:07 -0300

    feat: improve navbar style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import { Navbar } from '@/components/NavBar'
-import './globals.css'
 import type { Metadata } from 'next'
+import './globals.css'
+import { Navbar } from '@/components/NavBar'
 import { Footer } from '@/components/Footer'
 
 export const metadata: Metadata = {
@@ -15,10 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="flex h-screen flex-col items-center bg-zinc-50 px-6 py-4 dark:bg-port-black-900">
+      <body className="dark:bg-port-black-700 flex h-screen flex-col items-center bg-zinc-50 px-6 py-4">
         <div className="flex h-full w-full max-w-7xl flex-col">
           <Navbar />
-          <main className="flex flex-1 flex-col pt-20">{children}</main>
+          <main className="flex flex-1 flex-col pt-20 lg:pt-0">{children}</main>
           <Footer />
         </div>
       </body>
